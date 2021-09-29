ddd3e63506a9819bbc7a01b7833b2ecb25a5bc4f
Stefon Alfaro <45152948+stefonalfaro@users.noreply.github.com>
2021-09-28 18:31:08 -0700

    Initial commit

---
+++ ClassLibrary2/Barcodes.cs
@@ -0,0 +1,330 @@
+﻿using NBarCodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace Barcodes
+{
+    public class Image
+    {
+        public static System.Drawing.Image UPCA(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings{
+                Type = BarCodeType.Upca,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage()){
+                return barcodeImage;
+            }
+        }
+
+        public static System.Drawing.Image UPCE(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Upce,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                return barcodeImage;
+            }
+        }
+
+        public static System.Drawing.Image Code128(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Code128,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                return barcodeImage;
+            }
+        }
+
+        public static System.Drawing.Image Code39(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Code39,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                return barcodeImage;
+            }
+        }
+
+        public static System.Drawing.Image Interleaved25(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Interleaved25,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                return barcodeImage;
+            }
+        }
+
+        public static System.Drawing.Image PostNet(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.PostNet,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                return barcodeImage;
+            }
+        }
+
+        public static System.Drawing.Image Standard25(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Standard25,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                return barcodeImage;
+            }
+        }
+    }
+
+    public class Base64
+    {
+        public static string UPCA(string data)
+        {
+            return Convert.ToBase64String(Bytes.UPCA(data));
+        }
+
+        public static string Code128(string data)
+        {
+            return Convert.ToBase64String(Bytes.Code128(data));
+        }
+
+        public static string Ean13(string data)
+        {
+            return Convert.ToBase64String(Bytes.Ean13(data));
+        }
+
+        public static string Ean8(string data)
+        {
+            return Convert.ToBase64String(Bytes.Ean8(data));
+        }
+
+        public static string Interleaved25(string data)
+        {
+            return Convert.ToBase64String(Bytes.Interleaved25(data));
+        }
+
+        public static string PostNet(string data)
+        {
+            return Convert.ToBase64String(Bytes.PostNet(data));
+        }
+
+        public static string Standard25(string data)
+        {
+            return Convert.ToBase64String(Bytes.Standard25(data));
+        }
+
+        public static string UPCE(string data)
+        {
+            return Convert.ToBase64String(Bytes.UPCE(data));
+        }
+    }
+
+    public class Bytes
+    {
+        public static byte[] Code128(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Code128,
+                Data = data, 
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] UPCA(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Upca,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] Ean13(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Ean13,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] Ean8(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Ean8,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] Interleaved25(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Interleaved25,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] PostNet(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.PostNet,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] Standard25(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Standard25,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] UPCE(string data)
+        {
+            BarCodeSettings settings = new BarCodeSettings
+            {
+                Type = BarCodeType.Upce,
+                Data = data
+            };
+
+            BarCodeGenerator generator = new BarCodeGenerator(settings);
+
+            using (var barcodeImage = generator.GenerateImage())
+            {
+                using (var stream = new MemoryStream())
+                {
+                    barcodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    return stream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ ClassLibrary2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Barcodes")]
+[assembly: AssemblyDescription("Functions to generate Barcodes in Bytes, Base64, and Image")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GAPP Express Inc.")]
+[assembly: AssemblyProduct("Barcodes DLL")]
+[assembly: AssemblyCopyright("Copyright Stefon Alfaro ©  2021")]
+[assembly: AssemblyTrademark("Stefon Alfaro")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3bae190f-b317-44af-883b-bc99018bf1f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
